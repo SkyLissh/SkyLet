5fd005ce4ace161137d1b0bad79629c5810282e4
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-02-22 15:20:31 -0600

    :sparkles: Added new actions
    New actions was added to the action command

---
+++ app/commands.py
@@ -0,0 +1,14 @@
+from typing import List
+
+GIF_COMMANDS: List[str] = [
+    "hi",
+    "hug",
+    "kiss",
+    "punch",
+    "kick",
+    "kill",
+    "spank",
+    "shoot",
+    "slap",
+    "bite",
+]
