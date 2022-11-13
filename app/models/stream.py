b863c33b70202e60da2b0797cd08633149cc2fbc
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-11-13 02:57:36 -0600

    :bug: Image Caching
    Add a timestamp to image query to avoid show always the same image

--- app/models/stream.py
+++ app/models/stream.py
@@ -2,21 +2,35 @@ from datetime import datetime
 
 from pydantic import BaseModel, validator
 
+from app.utils.format_number import format_number
+
 
 class Stream(BaseModel):
     id: int
 
     user_id: int
     user_name: str
+    user_login: str
 
     game_name: str
 
     title: str
-    viewer_count: int
+    viewer_count: str
     thumbnail_url: str
 
     started_at: datetime
+    url: str
 
     @validator("thumbnail_url")
     def parse_thumbnail_url(cls, v: str) -> str:
-        return v.replace("{width}", "1920").replace("{height}", "1080")
+        dt = datetime.now()
+        v.replace("{width}", "1920").replace("{height}", "1080")
+        return f"{v}?{int(dt.timestamp())}"
+
+    @validator("viewer_count")
+    def parse_viewer_count(cls, v: int) -> str:
+        return format_number(v)
+
+    @validator("url")
+    def parse_url(cls, v: str, values: "Stream") -> str:
+        return f"https://twitch.tv/{values.user_login}"
