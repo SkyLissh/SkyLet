86501af41fe48470a0f6da115afffc6405852720
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-11-14 03:26:19 -0600

    :recycle: Custom Client
    Add a custom client to split logic and don't relie all in main file

---
+++ app/client.py
@@ -0,0 +1,69 @@
+import logging
+import platform
+import traceback
+
+import discord
+from discord.ext import commands as cmd
+from tabulate import tabulate
+
+from app.utils.embeds import error_embed
+
+ArgumentError = (
+    cmd.MissingRequiredArgument,
+    cmd.BadArgument,
+    cmd.TooManyArguments,
+    cmd.UserInputError,
+)
+
+
+class SkyLet(cmd.Bot):
+    log = logging.getLogger("app.skylet")
+    modules: list[str] = ["app.cogs.twitch.twitch", "app.cogs.mute.mute"]
+
+    async def on_ready(self) -> None:
+        print(
+            """\
+        ░██████╗██╗░░██╗██╗░░░██╗██╗░░░░░███████╗████████╗
+        ██╔════╝██║░██╔╝╚██╗░██╔╝██║░░░░░██╔════╝╚══██╔══╝
+        ╚█████╗░█████═╝░░╚████╔╝░██║░░░░░█████╗░░░░░██║░░░
+        ░╚═══██╗██╔═██╗░░░╚██╔╝░░██║░░░░░██╔══╝░░░░░██║░░░
+        ██████╔╝██║░╚██╗░░░██║░░░███████╗███████╗░░░██║░░░
+        ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝░░░╚═╝░░░
+        """
+        )
+        assert self.user is not None
+
+        table_rows = [
+            ["discord.py", f"v{discord.__version__}"],
+            ["python", f"v{platform.python_version()}"],
+            ["system", f"{platform.system()} v{platform.version()}"],
+            ["discord user", f"{self.user} (id: {self.user.id})"],
+            ["guilds", f"{len(self.guilds)}"],
+            ["users", f"{len(self.users)}"],
+        ]
+        self.log.info("\n" + tabulate(table_rows))
+
+    async def on_command_error(self, ctx: cmd.Context, error: Exception) -> None:
+        if isinstance(error, cmd.CommandNotFound):
+            await ctx.send(embed=error_embed("Command not found"))
+            return
+
+        if isinstance(error, ArgumentError):
+            await ctx.send(embed=error_embed("Missing required argument"))
+            return
+
+        if isinstance(error, cmd.MemberNotFound):
+            await ctx.send(embed=error_embed("Member not found"))
+            return
+
+        self.log.error(f"Error in command {ctx.command}: {error} - type: {type(error)}")
+        self.log.exception(
+            "".join(traceback.format_exception(type(error), error, error.__traceback__))
+        )
+
+    async def setup_hook(self) -> None:
+        for m in self.modules:
+            await self.load_extension(m)
+            self.log.info(f"Loaded module {m}")
+
+        self.log.info(f"Loaded {len(self.modules)} modules")
--- app/main.py
+++ app/main.py
@@ -1,74 +1,34 @@
 import logging
 import os
-import platform
-from typing import Union
+from typing import Optional
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from tabulate import tabulate
 
-from app.utils.embeds import error_embed
+from app.client import SkyLet
 
 load_dotenv()
+discord.utils.setup_logging()
 
 log: logging.Logger = logging.getLogger(__name__)
 
-PREFIX: str = os.getenv("DISCORD_PREFIX") or ">"
-DISCORD_TOKEN: Union[str, None] = os.getenv("DISCORD_TOKEN")
+PREFIX: str = os.getenv("DISCORD_PREFIX", ">")
+DISCORD_TOKEN: Optional[str] = os.getenv("DISCORD_TOKEN")
 
 if DISCORD_TOKEN is None:
     log.error("DISCORD_TOKEN is not set")
     exit(0)
 
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or(PREFIX), description="SkyLet discord bot"
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = SkyLet(
+    command_prefix=commands.when_mentioned_or(PREFIX),
+    description="SkyLet discord bot",
+    intents=intents,
 )
-
-modules: list[str] = ["app.cogs.twitch.twitch", "app.cogs.anti_spam"]
-
-# Load all modules
-for m in modules:
-    bot.load_extension(m)
-    log.debug(f"Loaded module {m}")
-
-log.info(f"Loaded {len(modules)} modules")
-
-
-@bot.event
-async def on_ready() -> None:
-    print(
-        """\
-    ░██████╗██╗░░██╗██╗░░░██╗██╗░░░░░███████╗████████╗
-    ██╔════╝██║░██╔╝╚██╗░██╔╝██║░░░░░██╔════╝╚══██╔══╝
-    ╚█████╗░█████═╝░░╚████╔╝░██║░░░░░█████╗░░░░░██║░░░
-    ░╚═══██╗██╔═██╗░░░╚██╔╝░░██║░░░░░██╔══╝░░░░░██║░░░
-    ██████╔╝██║░╚██╗░░░██║░░░███████╗███████╗░░░██║░░░
-    ╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚══════╝░░░╚═╝░░░
-    """
-    )
-
-    table_rows = [
-        ["discord.py", f"v{discord.__version__}"],
-        ["python", f"v{platform.python_version()}"],
-        ["system", f"{platform.system()} v{platform.version()}"],
-        ["discord user", f"{bot.user} (id: {bot.user.id})"],
-        ["guilds", f"{len(bot.guilds)}"],
-        ["users", f"{len(bot.users)}"],
-    ]
-    log.info("\n" + tabulate(table_rows))
-
-
-@bot.event
-async def on_command_error(ctx: commands.Context, error: Exception) -> None:
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send(embed=error_embed("Command not found"))
-        return
-
-    log.error(f"Error in command {ctx.command}: {error}")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-
     bot.run(DISCORD_TOKEN)
